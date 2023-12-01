c48c09c92fc36ecda86cdf4f35804a37a1f17640
Blue-Giant <42233512+Blue-Giant@users.noreply.github.com>
2023-12-02 00:12:40 +0800

    Add files via upload

---
+++ Results/case1_DNN_T_only/case2_DNN_X_only/11m_29d_16h_9m_55s/plot_solu_err.m
@@ -0,0 +1,93 @@
+clear all
+clc
+close all
+% 参数设置
+ws = 0.00001;
+T = 1800; % 时间范围
+Z = 10;   % 深度范围
+dt = 1.8;   % 时间步长
+dz = 0.1; % 深度步长
+
+% 网格设置
+timesteps = 0:dt:T;
+depths = 0:dz:Z;
+nt = length(timesteps);
+nz = length(depths);
+
+data2solus = load('test_solus.mat');
+solu2fdm = double(data2solus.Utrue);
+solu2dnn = double(data2solus.Usin);
+
+
+[meshX, meshY] = meshgrid(timesteps, depths);
+surfX = meshX(:, 1:10:1001);
+surfY = meshY(:, 1:10:1001);
+
+
+% 可视化结果
+figure('name','DNN_Solu')
+surf(surfY, surfX, solu2dnn);
+ylim([0,1800])
+xlim([0,10])
+zlim([-0.05, 1])
+ylabel('Time(s)', 'Interpreter', 'latex');
+xlabel('$z$ (m)', 'Interpreter', 'latex');
+zlabel('C($z$, $t$)', 'Interpreter', 'latex');
+% title('S-NPINN Solution');
+hold on
+% colorbar;
+hold on
+set(gca, 'XMinortick', 'off', 'YMinorTick', 'off', 'Fontsize', 15);
+set(gcf, 'Renderer', 'zbuffer');
+hold on
+
+figure('name','FDM_Solu')
+surf(surfY, surfX, solu2fdm);
+ylim([0,1800])
+xlim([0,10])
+ylabel('Time(s)', 'Interpreter', 'latex');
+xlabel('$z$ (m)', 'Interpreter', 'latex');
+zlabel('C($z$, $t$)', 'Interpreter', 'latex');
+% title('Analytical Solution');
+hold on
+% colorbar;
+hold on
+set(gca, 'XMinortick', 'off', 'YMinorTick', 'off', 'Fontsize', 15);
+set(gcf, 'Renderer', 'zbuffer');
+hold on
+
+abs_err = abs(solu2dnn - solu2fdm);
+% figure('name','Absolute Error')
+% surf(surfY, surfX, abs_err, 'EdgeColor', 'none');
+% xlim([0,10])
+% ylim([0,1800])
+% ylabel('Time (s)');
+% xlabel('z(m)');
+% zlabel('C(z,t)');
+% title('Absolute Error', 'position', [11,1000]);
+% hold on
+% cb2 = colorbar();
+% set(get(cb2,'title'),'string','g/L','FontSize',16, 'position', [50, 230]);
+% caxis([0 0.007])
+% hold on
+% set(gca, 'XMinortick', 'off', 'YMinorTick', 'off', 'Fontsize', 15);
+% set(gcf, 'Renderer', 'zbuffer');
+% hold on
+
+figure('name','Absolute Error2')
+surf(surfX, surfY, abs_err, 'EdgeColor', 'none');
+xlim([0,1800])
+ylim([0,10])
+xlabel('Time[s]', 'Interpreter', 'latex');
+ylabel('$z$ [m]', 'Interpreter', 'latex');
+zlabel('C($z$, $t$)~~[g/L]', 'Interpreter', 'latex');
+title('Absolute Error [g/L]', 'Interpreter', 'latex');
+hold on
+cb2 = colorbar();
+% set(get(cb2,'title'),'string','g/L','FontSize',16, 'position', [50, 220]);
+caxis([0 0.5])
+hold on
+set(gca, 'XMinortick', 'off', 'YMinorTick', 'off', 'Fontsize', 15);
+set(gcf, 'Renderer', 'zbuffer');
+hold on
+
---
+++ Results/case1_DNN_T_only/case2_mse_rel/plot_rel.m
@@ -0,0 +1,63 @@
+clc
+clear all
+close all
+
+dataErr2case1_DNN = load('test_Err2sin_case2_DNN.mat');
+% mse2case1_DNN = sqrt(dataErr2case1_DNN.mse);
+% rel2case1_DNN = sqrt(dataErr2case1_DNN.rel);
+
+mse2case1_DNN = (dataErr2case1_DNN.mse);
+rel2case1_DNN = (dataErr2case1_DNN.rel);
+
+dataErr2case1_T = load('test_Err2sin_cases2_T.mat');
+% mse2case1_T = sqrt(dataErr2case1_T.mse);
+% rel2case1_T = sqrt(dataErr2case1_T.rel);
+mse2case1_T = (dataErr2case1_T.mse);
+rel2case1_T = (dataErr2case1_T.rel);
+
+dataErr2case1_X = load('test_Err2sin_cases2_X.mat');
+% mse2case1_X = sqrt(dataErr2case1_X.mse);
+% rel2case1_X = sqrt(dataErr2case1_X.rel);
+mse2case1_X = (dataErr2case1_X.mse);
+rel2case1_X = (dataErr2case1_X.rel);
+
+dataErr2case1_Both = load('test_Err2sin_cases2_XT.mat');
+% mse2case1_Both = sqrt(dataErr2case1_Both.mse);
+% rel2case1_Both = sqrt(dataErr2case1_Both.rel);
+mse2case1_Both = (dataErr2case1_Both.mse);
+rel2case1_Both = (dataErr2case1_Both.rel);
+
+epoch = 1:51;
+
+figure('name','error')
+fig_rel2case1_DNN = plot(epoch,rel2case1_DNN,'r-*', 'linewidth',2);
+hold on
+
+bb = [0 0.55 0.8];
+fig_rel2case1_T = plot(epoch, rel2case1_T,'-X', 'linewidth',2, 'color', bb);
+hold on
+
+fig_rel2case1_X = plot(epoch,rel2case1_X,'m:', 'linewidth',2);
+hold on
+
+fig_rel2case1_Both = plot(epoch,rel2case1_Both,'c-V', 'linewidth',2);
+hold on
+
+set(gca,'yscale','log')
+set(gca, 'Fontsize', 18)
+xlabel('epoch/1000', 'Fontsize', 18)
+ylabel('REL', 'Fontsize', 18, 'Interpreter', 'latex')
+xlim([0,51])
+ylim([0.0001, 100])
+hold on
+% 
+lgd0=legend([fig_rel2case1_DNN,fig_rel2case1_T],{'PINN','T-NPINN'},'orientation','horizontal','location','North');
+set(lgd0,'FontSize',18.5);
+lgd0.Position = [0.3  0.820  0.45  0.2];
+legend boxoff;
+
+ah1=axes('position',get(gca,'position'),'visible','off');
+lgd1=legend(ah1,[fig_rel2case1_X,fig_rel2case1_Both],{'S-NPINN','ST-NPINN'},'orientation','horizontal','location','North');
+set(lgd1,'FontSize',18.5);
+lgd1.Position = [0.3  0.77  0.45  0.2];
+legend boxoff;
